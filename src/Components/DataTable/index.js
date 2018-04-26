314338ac423d11ac6e1b8654ae196f16950674ab
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-04-26 14:38:12 +0530

    Feature:  Added sorting

--- src/Components/DataTable/index.js
+++ src/Components/DataTable/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './datatable.css';
 
 export default class DataTable extends React.Component {
@@ -7,7 +9,9 @@ export default class DataTable extends React.Component {
 
         this.state = {
             headers: props.headers,
-            data: props.data
+            data: props.data,
+            sortby: null,
+            descending: null,
         }
 
         this.keyField = props.keyField || "id"; // TODO: revisit this logic
@@ -27,12 +31,16 @@ export default class DataTable extends React.Component {
             let cleanTitle = header.accessor;
             let width = header.width;
 
+            if (this.state.sortby === index) {
+                title += this.state.descending ? '\u2193': '\u2191';
+            }
+
             return (
                 <th key={cleanTitle}
                     ref={(th)=>this.th = th}
-                    style={{widht: width}}
+                    style={{width: width}}
                     data-col={cleanTitle}>
-                    <span className="header-cell">
+                    <span data-col={cleanTitle} className="header-cell">
                         {title}
                     </span>
                 </th>
@@ -83,6 +91,33 @@ export default class DataTable extends React.Component {
         return contentView;
     }
     
+    onSort = (e) => {
+        let data = this.state.data.slice(); // Give new array
+        let colIndex = ReactDOM.findDOMNode(e.target).parentNode.cellIndex;
+        let colTitle = e.target.dataset.col;
+        
+        let descending = !this.state.descending;
+
+        data.sort((a,b) => {
+            let sortVal = 0;
+            if (a[colTitle] < b[colTitle]) {
+                sortVal = -1;
+            } else if (a[colTitle] > b[colTitle]) {
+                sortVal = 1;
+            }
+            if (descending) {
+                sortVal = sortVal * -1;
+            }
+            return sortVal;
+        });
+
+        this.setState({
+            data,
+            sortby: colIndex,
+            descending
+        });
+    }
+
     renderTable = () => {
         let title = this.props.title || "DataTable";
         let headerView = this.renderTableHeader();
@@ -95,7 +130,7 @@ export default class DataTable extends React.Component {
                 <caption className="data-table-caption">
                     {title}
                 </caption>
-                <thead>
+                <thead onClick={this.onSort}>
                     <tr>
                         {headerView}
                     </tr>
